ce42ad92bd6dddf6efb56fee5fb3235211b40395
93shrx <vabjr@yahoo.com.br>
2026-06-02 23:25:27 -0300

    Initial commit

---
+++ src/main/java/com/test/xbraintest/XBrainTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.xbraintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XBrainTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XBrainTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/xbraintest/XBrainTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.xbraintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XBrainTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
